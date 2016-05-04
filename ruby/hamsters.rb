dc392be45c210e1af72e5d6943b7b1f784686413
“Greg <huber01@greghuber.com>
2016-05-04 11:27:58 -0500

    Add initial questions and convert answers to integers where appropriate

---
+++ ruby/hamsters.rb
@@ -0,0 +1,15 @@
+puts "What's the hamster's name?"
+name = gets.chomp
+
+puts "On a scale of 1 to 10, how loud is the hamster?"
+volume = gets.to_i
+
+puts "What color is its fur?"
+color = gets.chomp
+
+puts "Is the hamster adoptable?"
+adoptable = gets.chomp
+
+puts "How old do you estimate the hampster to be?"
+age = gets.to_i
+
