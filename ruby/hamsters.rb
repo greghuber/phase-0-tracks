96b60125cb6d875f9de099d69a15a846deefb622
“Greg <huber01@greghuber.com>
2016-05-04 11:50:01 -0500

    Add print out of data to console

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -10,6 +10,24 @@ color = gets.chomp
 puts "Is the hamster adoptable?"
 adoptable = gets.chomp
 
-puts "How old do you estimate the hampster to be?"
-age = gets.to_i
+if adoptable == "yes" || adoptable == "Yes"
+	adoptable = "The hamster is adoptable."
+else
+	adoptable = "No, the hamster is not adoptable."
+end
+
+puts "How old do you estimate the hampster to be?  If not sure, hit return."
+response = gets.chomp
+
+if response != ""
+	age = response.to_i
+else
+	age = nil
+end
+
+puts "Name: #{name}"
+puts "Estimate Age: #{age}"
+puts "Color: #{color}"
+puts "Volume: #{volume} out of 10"
+puts "Adoptability: #{adoptable}"
 
