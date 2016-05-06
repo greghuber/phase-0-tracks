ed023e9a7849e7238511aedec5332c05c236d53f
“Greg <huber01@greghuber.com>
2016-05-06 10:58:26 -0500

    Add initial questions for new employee

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+#Ask new employee questions
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+birthyear = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+health = gets.chomp
