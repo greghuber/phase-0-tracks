f9a12eea7f4f788a31c903dbb238014fd7c54a38
“Greg <huber01@greghuber.com>
2016-05-06 16:14:05 -0500

    Write decrypt method and test both methods

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -5,7 +5,10 @@
 def encrypt(code)
 index = 0
 	while index < code.length
-		if code[index] != " "
+		if code[index] == " "
+		elsif code[index] == "z"
+			code[index] = "a"
+		else
 			code[index] = code[index].next!
 		end
 		index += 1
@@ -13,23 +16,29 @@ index = 0
 	return code
 end
 
-puts "What is your password?"
-code = gets.chomp
-puts encrypt(code)
-
-
-
-
 #Create method to decrypt that reverses the process of .encrypt
 
+def decrypt(code)
+index = 0
+	while index < code.length
+		if code[index] == " "
+		elsif code[index] == "a"
+			code[index] = "z"
+		else
+			current = "abcdefghijklmnopqrstuvwxyz".index(code[index])
+			code[index] = "abcdefghijklmnopqrstuvwxyz"[current - 1]	
+		end
+		index += 1
+	end
+	return code
+end
 
-#def encrypt(code)
-#index = 0
-#	while index < code.length
-#		if code[index] != " "
-#			code[index] = code[index].next
-#		end
-#		index += 1
-#	end
-#end
+puts "What is your password?"
+password = gets.chomp
+puts encrypt(password)
+
+puts "What is the encrypted password?"
+scrambled = gets.chomp
+puts decrypt(scrambled)
+
 
