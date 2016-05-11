509d7b0c3915102f54c9807e28c8a8d73c2a6f0c
“Greg <huber01@greghuber.com>
2016-05-11 12:54:59 -0500

    Add methods filtering arrays and hashes

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,4 +1,7 @@
-# Release 0
+# RELEASE 0
+puts " "
+puts "RELEASE 0"
+puts " "
 
 def add_numbers
 	number01 = 5
@@ -10,7 +13,10 @@ end
 
 add_numbers { |number01, number02| puts "#{number01} plus #{number02} is equal to #{number01 + number02}."}
 
-# Release 1
+# RELEASE 1
+puts " "
+puts "RELEASE 1"
+puts " "
 
 cities = ["Chicago", "New York", "Denver", "Detroit"]
 
@@ -22,7 +28,7 @@ p cities
 cities.each { |city| puts city }
 p cities
 
-# array using .map
+# array using .map!
 puts "Using .map on an array"
 p cities
 cities.map! do |city| 
@@ -37,6 +43,72 @@ mascots.each { |school, team| puts school, team }
 p mascots
 
 
+mascots.delete_if {|school, team| team[0] == "b" }
+
+#RELEASE 2
+
+#Run hashes through four requirements
+puts " "
+puts "RELEASE 2: HASH SECTION"
+puts " "
+
+hash_01 = {k1: 0, k2: 2, k3: 5}
+puts hash_01
+
+puts ""
+puts "Using delete_if"
+hash_01 = {k1: 0, k2: 2, k3: 5}
+hash_01.delete_if {|k,v| v < 1}
+puts hash_01
+
+puts ""
+puts "Using reject!"
+hash_01 = {k1: 0, k2: 2, k3: 5}
+hash_01.reject! {|k,v| v < 1}
+puts hash_01
+
+puts ""
+puts "Using select!"
+hash_01 = {k1: 0, k2: 2, k3: 5}
+hash_01.select! {|k,v| v > 1}
+puts hash_01
+
+puts ""
+puts "Using keep_if"
+hash_01 = {k1: 0, k2: 2, k3: 5}
+hash_01.keep_if {|k,v| v < 5}
+puts hash_01
+
+#Run arrays through four requirements
+puts " "
+puts "RELEASE 2: ARRAY SECTION"
+
+puts " "
+puts "Using .delete_if"
+cities = ["Chicago", "New York", "Denver", "Detroit"]
+cities.delete_if {|city| city[0] == "D" }
+puts cities
+
+puts " "
+puts "Using .reject!"
+cities = ["Chicago", "New York", "Denver", "Detroit"]
+cities.reject! {|city| city.length > 6}
+puts cities
+
+puts " "
+puts "Using .select!"
+cities = ["Chicago", "New York", "Denver", "Detroit"]
+cities.select! {|city| city.length > 6}
+puts cities
+
+puts " "
+puts "Using .keep_if"
+cities = ["Chicago", "New York", "Denver", "Detroit"]
+cities.keep_if {|city| city.length < 7}
+puts cities
+
+   
+ 
 
 
 
