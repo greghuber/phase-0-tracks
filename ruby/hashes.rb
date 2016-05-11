85727688b94b42ecddbbbaec97f203e5997718b6
“Greg <huber01@greghuber.com>
2016-05-10 22:26:13 -0500

    Add code to allow user to correct one mistake

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,6 +1,6 @@
 # Program to allow interior designer to enter details of a given client:
 
-# Format responses to input questions so that:
+# Convert responses to input questions so that:
 # name, city, and decor theme are strings,
 # age, number of chilren, and sq ft of home are integers, and
 # whether the client was a referral, whether the home is a single family as 
@@ -10,7 +10,7 @@
 
 client_details = {}
 
-# Ask user questions, formatting responses and populating the hash as you go
+# Ask user questions, converting responses and populating the hash as you go
 
 puts "What is the client's name?"
 client_details[:name] = gets.chomp
@@ -51,4 +51,39 @@ else
 	client_details[:feng_shui] = false
 end
 
+# Print hash to screen
 p client_details
+
+# Ask user if they made a mistake, and if so what category. 
+# Convert the response to a symbol representing the proper key
+# Set a variable equal to the key to take into the conditional check
+
+puts "Did you make any mistakes?  It's OK, it happens.\nEnter the category you misentered.  If none, enter 'none'."
+key = gets.chomp.to_sym
+
+# Based on user's response either do nothing or ask a question based on the 
+# need to convert the new response to a string, integer or boolean
+
+if key == :none
+elsif 
+	key == :name || key == :city || key == :decor_theme
+	puts "What is the correct entry?"
+	client_details[key] = gets.chomp
+elsif 
+	key == :age || key == :square_feet || key == :children
+	puts "What is the correct number?"
+	client_details[key] = gets.chomp.to_i
+else
+	puts "What is the correct entry, yes or no?"
+	if gets.chomp == "yes"
+	client_details[key] = true
+	else
+	client_details[key] = false
+	end
+end
+
+# Print hash to screen
+p client_details
+
+
+
