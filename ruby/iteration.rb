36182508adca7bb0c6057283aeff423f9e5115c3
“Greg <huber01@greghuber.com>
2016-05-11 12:02:53 -0500

    Add code to pass array through .each and .map! methods and hash through .each

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,5 @@
+# Release 0
+
 def add_numbers
 	number01 = 5
 	number02 = 3
@@ -7,5 +9,37 @@ def add_numbers
 end
 
 add_numbers { |number01, number02| puts "#{number01} plus #{number02} is equal to #{number01 + number02}."}
+
+# Release 1
+
+cities = ["Chicago", "New York", "Denver", "Detroit"]
+
+mascots = {michigan: "wolverines", wisonsin: "badgers", colorado: "buffalos"}
+
+# array using .each
+puts "Using .each on an array"
+p cities
+cities.each { |city| puts city }
+p cities
+
+# array using .map
+puts "Using .map on an array"
+p cities
+cities.map! do |city| 
+	city.upcase
+end
+p cities
+
+# hash using .each
+puts "Using .each on a hash"
+p mascots
+mascots.each { |school, team| puts school, team }
+p mascots
+
+
+
+
+
+
 
 
