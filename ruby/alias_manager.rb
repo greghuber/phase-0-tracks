a1dfbbc35e2fb2dee19890bfecc8c784c05de2a7
“Greg <huber01@greghuber.com>
2016-05-13 09:12:28 -0500

    Put code in def method form and call to confirm function

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -7,49 +7,50 @@
 # Example input: "Felicia Torres" Example output: "Vussit Gimodoe"
 #________________________________________________________________________
 
-
-# Turn real name into array to reverse items.  Rejoin into string and
-# make all letters lowercase to process.  Assign to variable swap_name.
-
-real_name = "Felicia Torres"
-# TEST p real_name
-swap_name = real_name.split(' ').reverse.join(' ').downcase
-# TEST swap_name
-
-# Set variables to check for vowels and consonants, including edge cases.
-# Set loop counter to 0.
-vowels = "aeioua"
-consonants = "bcdfghjklmnpqrstvwxyzb"
-counter = 0
-
-# Iterate through swap_name applying rules for spaces, vowels and consonants.
-
-while counter < swap_name.length
-
-	if swap_name[counter] == " "
+def alias_name(real_name)
+	# Turn real name into array to reverse items.  Rejoin into string and
+	# make all letters lowercase to process.  Assign to variable swap_name.
 	
-	elsif (swap_name[counter] == "a") || (swap_name[counter] == "e") || (swap_name[counter] == "i") || (swap_name[counter] == "o") || (swap_name[counter] == "u")
-		current = vowels.index(swap_name[counter])
-		swap_name[counter] = vowels[current + 1]
+	# TEST real_name = "Felicia Torres"
+	# TEST p real_name
+	swap_name = real_name.split(' ').reverse.join(' ').downcase
+	# TEST swap_name
+	
+	# Set variables to check for vowels and consonants, including edge cases.
+	# Set loop counter to 0.
+	vowels = "aeioua"
+	consonants = "bcdfghjklmnpqrstvwxyzb"
+	counter = 0
+	
+	# Iterate through swap_name applying rules for spaces, vowels and consonants.
+	
+	while counter < swap_name.length
+	
+		if swap_name[counter] == " "
 		
-	else
-		current = consonants.index(swap_name[counter])
-		swap_name[counter] = consonants[current + 1]
-				
+		elsif (swap_name[counter] == "a") || (swap_name[counter] == "e") || (swap_name[counter] == "i") || (swap_name[counter] == "o") || (swap_name[counter] == "u")
+			current = vowels.index(swap_name[counter])
+			swap_name[counter] = vowels[current + 1]
+			
+		else
+			current = consonants.index(swap_name[counter])
+			swap_name[counter] = consonants[current + 1]
+					
+		end
+	
+	counter += 1
 	end
-
-counter += 1
+	# TEST swap_name
+	
+	# Turn revised swap_name into array to capitalize each item then rejoin
+	# into string.  Assign to variable code_name to return.
+	
+	code_name = swap_name.split(' ').map {|x| x.capitalize }.join(' ')
+	
+	return code_name
 end
-# TEST swap_name
 
-# Turn revised swap_name into array to capitalize each item then rejoin
-# into string.  Assign to variable code_name to return.
-
-code_name = swap_name.split(' ').map {|x| x.capitalize }.join(' ')
-
-p code_name
-
-
+puts alias_name("Felicia Torres")
 
 
 
