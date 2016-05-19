ca01d1d38122e9ac3cb8a64ef965c25a082691f3
“Greg <huber01@greghuber.com>
2016-05-19 12:27:35 -0500

    Write code to define class and set attributes, also code for methods to change attributes, and driver code to test.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -17,16 +17,68 @@
 # Similarly, allow color and bling read/write priviledges to outside of
 # class, but age and functionality read only.
 
+class Bicylcle
+  attr_reader :age, :functionality
+  attr_accessor :color, :bling	
+
+  def initialize(color, bling)
+    puts "Initializing Bicylcle instance ..."
+    @color = color
+    @age = 0
+    @bling = bling
+    @functionality = true
+  end
+
+  
+# Similarly, allow color and bling read/write priviledges to outside of
+# class, but age and functionality read only.
+
 # Define method paint_job to take new color as parameter and assign that
 # to @color, printing the new status of the bike's color.
+
+  def paint_job(new_color)
+    @color = new_color
+    puts "The bike is now sporting the color #{@color}!"
+  end
 
 # Define method add_bling to take new color as parameter and assign that
 # to @color, printing the new status of the bike's color.
 
+  def add_bling(new_bling)
+    @bling << new_bling
+    puts "The bike now has #{@bling.join(', ')} for bling."
+  end
+
 # Define method plus_year which takes no parameter, but increments the
 # bike's age one year, then prints the new status of the bike's age.
+
+  def plus_year
+    @age += 1
+    puts "The bike is now #{@age} years old."
+  end
 
 # Define method mechanic which takes no parameter, but describes a bike
 # entering or leaving a shop, that is was working, now is not or 
 # vice versa, then  prints the new status of the bike's functionality.
+
+  def mechanic
+    if functionality
+    	functionality = false
+    else
+    	functionality = true
+    end
+
+    if functionality
+      puts "The bike is working again!"
+    else
+      puts "The bike is in the shop! :("
+    end
+  end
+end
+
+bike = Bicylcle.new("red", ["horn", "reflectors", "streamers"])
+
+bike.add_bling("aerobars")
+
+
 
