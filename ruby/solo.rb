fc88179b7b5a742bba462e0cd0294fb91af566d6
“Greg <huber01@greghuber.com>
2016-05-19 12:08:42 -0500

    Pseudocode only, design of class Bicycle, describing attributes and methods.

---
+++ ruby/solo.rb
@@ -0,0 +1,32 @@
+# DESIGN A CLASS: BICYCLE which must have...
+#
+# at least 3 ATTRIBUTES (using at least two data types) whose values 
+# will vary from instance to instance (example: age):
+# color (string), age (integer), functional (boolean) &
+# bling (array of strings),
+#
+# and at least 3 METHODS, at least one of which takes an argument:
+# paint_job (change color, takes new_color as parameter), add_bling 
+# (takes new_bling), plus_year (increment age, no parameter) &
+# mechanic (change status of functionality, no parameter)
+#__________________________________________________________
+
+# Initialize new instance with parameters color and bling as input.
+# Set age to '0' and functionality to 'true' by default
+
+# Similarly, allow color and bling read/write priviledges to outside of
+# class, but age and functionality read only.
+
+# Define method paint_job to take new color as parameter and assign that
+# to @color, printing the new status of the bike's color.
+
+# Define method add_bling to take new color as parameter and assign that
+# to @color, printing the new status of the bike's color.
+
+# Define method plus_year which takes no parameter, but increments the
+# bike's age one year, then prints the new status of the bike's age.
+
+# Define method mechanic which takes no parameter, but describes a bike
+# entering or leaving a shop, that is was working, now is not or 
+# vice versa, then  prints the new status of the bike's functionality.
+
