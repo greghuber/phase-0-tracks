dced51c3c463339abffd6c2c67dbfcab274e1667
Garret Bernstein <gbernz92@gmail.com>
2016-05-20 15:57:52 -0500

    Added new commands for puppy, and created new class 'Computer.' Initial commit.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,82 @@ class Puppy
     toy
   end
 
+  def speak(num)
+  	num.times {puts "Woof!"}
+  end
+
+  def roll_over
+  	puts "*Rolls over*"
+  end
+
+  def dog_years(age)
+  	puts age * 7
+  end
+
+  def play_dead
+  	puts "*He begins to play dead*"
+  end
+
+  def initialize
+  	puts "Initializing new puppy instance..."
+  end
 end
+#--------------------------------
+class Computer
+	
+	def initialize
+		puts "Initializing new computer build"
+	end
+
+	def intel(core)
+		puts "You have installed an Intel #{core} processor."
+		
+	end
+
+	def overall_price(price)
+		puts "Your computer cost #{price} dollars."
+	end
+end
+	
+computer_array = []
+loop_num = 0 
+
+while loop_num < 50
+	computer_array.push(Computer.new)
+	loop_num += 1
+end
+
+computer_array.each do |component|
+	component.intel("i7")
+	component.overall_price("one million")
+end
+#--------------------------------
+#Driver code for class computer 
+GB = Computer.new
+GB.intel("i3")
+GB.overall_price("two-hundred")
+
+#--------------------------------
+#Driver code for class puppies
+Coco = Puppy.new
+
+#Fetch
+puts "~~Testing fetch~~"
+Coco.fetch("ball")
+
+#Speak
+puts "~~Testing speak~~"
+Coco.speak(3)
+
+#Roll-over
+puts "~~Testing rolls_over~~"
+Coco.roll_over
+
+#Dog years
+puts "~~Testing dog years~~"
+Coco.dog_years(5)
+
+#Play dead
+puts "~~Testing play dead~~"
+Coco.play_dead
 
