ab930e713f2792b293406b40b48bad3dd4eb3a2e
“Greg <huber01@greghuber.com>
2016-05-20 07:41:38 -0500

    Add user interface to collect data and print out all bikes added.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,24 +1,23 @@
-# DESIGN A CLASS: BICYCLE which must have...
+# DESIGN A CLASS: BICYCLE which will have...
 #
-# at least 3 ATTRIBUTES (using at least two data types) whose values 
-# will vary from instance to instance (example: age):
+# at least 3 ATTRIBUTES (using at least two data types):
 # color (string), age (integer), functional (boolean) &
 # bling (array of strings),
 #
 # and at least 3 METHODS, at least one of which takes an argument:
 # paint_job (change color, takes new_color as parameter), add_bling 
 # (takes new_bling), plus_year (increment age, no parameter) &
-# mechanic (change status of functionality, no parameter)
+# mechanic (change status of functional, no parameter)
 #__________________________________________________________
 
 # Initialize new instance with parameters color and bling as input.
-# Set age to '0' and functionality to 'true' by default
+# Set age to '0' and functional to 'true' by default
 
 # Similarly, allow color and bling read/write priviledges to outside of
-# class, but age and functionality read only.
+# class, but age and functional read only.
 
 class Bicylcle
-  attr_reader :age, :functionality
+  attr_reader :age, :functional
   attr_accessor :color, :bling	
 
   def initialize(color, bling)
@@ -26,12 +25,8 @@ class Bicylcle
     @color = color
     @age = 0
     @bling = bling
-    @functionality = true
+    @functional = true
   end
-
-  
-# Similarly, allow color and bling read/write priviledges to outside of
-# class, but age and functionality read only.
 
 # Define method paint_job to take new color as parameter and assign that
 # to @color, printing the new status of the bike's color.
@@ -58,30 +53,122 @@ class Bicylcle
   end
 
 # Define method mechanic which takes no parameter, but describes a bike
-# entering or leaving a shop, that is was working, now is not, or 
-# vice versa, then prints the new status of the bike's functionality.
+# entering or leaving a shop, i.e., was working, now is not, or 
+# vice versa, then prints the new status of the bike's functional.
 
   def mechanic
-    if @functionality
-    	@functionality = false
+    if @functional
+    	@functional = false
     else
-    	@functionality = true
+    	@functional = true
     end
 
-    if @functionality
+    if @functional
       puts "The bike is working again!"
     else
       puts "The bike is in the shop! :("
     end
   end
+
+# Create method to group data into a hash for easy print-out
+
+  def group_data
+    data_hash = {color: @color, bling: @bling, age: @age, functional: @functional}
+  end
+     
 end
 
-bike = Bicylcle.new("red", ["horn", "reflectors", "streamers"])
+#__________________________________________________________
+# DRIVER CODE to verify
 
-bike.add_bling("aerobars")
-bike.plus_year
-bike.mechanic
-bike.paint_job("orange")
-bike.mechanic
+# bike = Bicylcle.new("red", ["horn", "reflectors", "streamers"])
+
+# bike.add_bling("aerobars")
+# bike.plus_year
+# bike.mechanic
+# bike.paint_job("orange")
+# bike.mechanic
+#__________________________________________________________
+
+# USER INTERFACE  Ask user if they want to create a new bike.
+# Until they answer no, ask them what color and decorations they
+# want.  Store decorations in an array.  Create the bike and store
+# new bike info in an array 'new_bikes'.
+
+new_bikes = []
+answer = nil
+until answer == "no"
+
+  puts "Would you like to create a new bike?  Type 'yes' or 'no'"
+  answer = gets.chomp
+
+    if answer == "yes"
+
+# Ask for color and store in string variable 'color'.
+
+      puts "What color would you like it to be?"
+      color = gets.chomp
+
+# Ask for decorations until user indicates they are done.
+# Store in array 'bling'.
+
+      bling = nil
+      bling_set = []
+
+      until bling == "done"
+        puts "What decorative accessories would you like it to have?"
+        puts "Type 'done' when done."
+        bling = gets.chomp
+        break if bling == "done"
+
+        bling_set << bling
+      end
+
+# Create new bike, add data hash to new_bikes array
+
+      bike = Bicylcle.new(color, bling_set)
+
+      new_bikes << bike.group_data
+      
+
+    elsif answer == "no"
+      puts "Alright, that's enough bikes for now."
+      puts " "
+
+    else
+      puts "I didn't understand your answer."
+      puts "Please type 'yes' or 'no'." 
+    end 
+end        
+
+#__________________________________________________________
+# Print out characteristics of new bikes created
+# Print bling array in plain english
+p new_bikes
+
+puts "Here are the new bikes you created:"
+puts " "
+
+counter = 0
+
+until counter == new_bikes.length
+  puts "Bike Number #{counter + 1}"
+
+  new_bikes[counter].each do |key, value|
+    if key == :bling
+        if value == []
+          puts "#{key}: none"
+        else
+          puts "#{key}: #{value.join(', ')}"
+        end  
+    else
+      puts "#{key}: #{value}"
+    end
+  end
+  puts " "
+  counter += 1  
+end
+
+
 
 
