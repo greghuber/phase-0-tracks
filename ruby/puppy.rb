126f815c831c2b712d0b59c7b17b6f7d8f4e6ad4
Garret Bernstein <gbernz92@gmail.com>
2016-05-20 08:57:48 -0500

    Added class information for Puppies; initial commit.

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+SPECIES ------------------------
+Canin lupus familiaris
+
+CHARACTERISTICS ----------------
+Breed: Maltese
+Friendly: No
+Hair or Fur: Hair
+Name: Chip
+Color: Snow white
+
+BEHAVIOR -----------------------
+Bark 
+Play
+Hide Toys
