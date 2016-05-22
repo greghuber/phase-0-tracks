aa79c0b8eea9facd2f00260e118c8f781a93c528
Garret Bernstein <gbernz92@gmail.com>
2016-05-22 17:02:40 -0500

    quick commit

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,12 @@
 class Santa
 
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
@@ -8,13 +15,26 @@ class Santa
 		puts "That was a good #{cookie} cookie!"
 	end
 
-	def initialize
-		puts "initializing Santa instance..."
+	def celebrate_birthday
+		age + 1
+	end
+
+	def instance
+		puts "Santa is #{@age} years old."
 	end
 end
 
-#Driver code
-Test_santa = Santa.new
-Test_santa.eat_cookies("chocolate-chip")
-Test_santa.speak
+santas = {}
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+santas.each do |santa|
+	puts "Initializing Santa instance #{santas.length}..."
+	Santa.new = santas >> {santa.length => [example_genders, example_ethnicities]}
+	puts "There are now #{santas.length}."
+end
+
+# #Driver code
+# Test_santa = Santa.new
+# Test_santa.eat_cookies("chocolate-chip")
+# Test_santa.speak
 
---
+++ ruby/testalias.rb
@@ -0,0 +1,18 @@
+# def alias_machine(full_name)
+# 	full_name = full_name.downcase
+# 	vowels = ['a','e','i','o','u']
+# 	names = full_name.split(' ')
+# 	coded_name = []
+
+# 	names.each_with_index do |word, i|
+# 		if names.include vowels
+
+
+
+
+
+letters.map! {|letter| letter.next}
+	letters
+new_string = letters.join
+p "#{new_string}"
+end
