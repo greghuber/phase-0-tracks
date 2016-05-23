3450ddf239e47e1b5594c50ec18525e067b95f8e
Garret Bernstein <gbernz92@gmail.com>
2016-05-23 09:28:13 -0500

    added the attr reader and accessor components to the santa.rb file

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,39 +1,56 @@
 class Santa
 
-	def initialize(gender, ethnicity)
+	attr_reader :age, :ethnicity
+	attr_accessor :gender
+
+	def initialize(gender, ethnicity, cookies)
 		@gender = gender
 		@ethnicity = ethnicity
+		@cookies = cookies
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
+		
 	end
 
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 
-	def eat_cookies(cookie)
-		puts "That was a good #{cookie} cookie!"
+	def eat_cookies(cookies)
+		puts "That was a good #{cookies} cookie!"
 	end
 
 	def celebrate_birthday
-		age + 1
+		@age + 1
 	end
 
-	def instance
-		puts "Santa is #{@age} years old."
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete(reindeer)
+		@reindeer_ranking << reindeer
+		
 	end
 end
 
-santas = {}
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-santas.each do |santa|
-	puts "Initializing Santa instance #{santas.length}..."
-	Santa.new = santas >> {santa.length => [example_genders, example_ethnicities]}
-	puts "There are now #{santas.length}."
-end
+santas = []
+puts "How many Santas will be created?"
+num_loops = gets.chomp.to_i
 
-# #Driver code
+gender = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicity = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+cookies = ["chocolate-chip", "macadamia", "white-chocolate", "snickerdoodle", "oatmeal-raisin"]
+santa = Santa.new(gender, ethnicity, cookies)
+
+
+
+num_loops.times {|count| puts "#{santa.speak} Santa #{count + 1} is #{gender.sample}, #{ethnicity.sample} and he/she is #{age = rand(140)} years old. Santa says that was a good #{cookies.sample}!"}
+
+#Creates Santas with new gender and ethnicity based off given arrays
+#genders.length.times do |i|
+#   puts "Initializing Santa instance #{santas.length}..."
+# 	santas << Santa.new(#{genders[i]}, #{ethnicities[i])
+#end
+
+#Driver code for various Santa methods
 # Test_santa = Santa.new
 # Test_santa.eat_cookies("chocolate-chip")
 # Test_santa.speak
