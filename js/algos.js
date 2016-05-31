f2736052eb76be463f2275d4665a60f8445f7add
“Greg <huber01@greghuber.com>
2016-05-31 09:24:45 -0500

    Write function to check for k-v pair match between two objects.

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,4 @@
+// RELEASE 0:
 // Write a function that takes an array of words/phrases and
 // returns the longest word in the array.
 // 
@@ -51,15 +52,99 @@ function longestString(array) {
   return longest;
 }
 
-// DRIVER TEST CODE
-
-console.log(longestString(["abc", "defghi", "jklmnopqr"]));
-
-console.log(longestString(["abc", "defghi", "jklmno"]));
-
-console.log(longestString(["Ruby", "I like to code!", "JavaScript"]));
 
 
 
+
+// RELEASE !:
+// Write a function that takes two objects and checks to see if they
+// share at least one key-value pair.
+
+// Step through the keys in the first object.  
+// At each step, step through the second object and compare k-v to 
+// that of the current k-v in the first (so compare all kv pairs 
+// in object 2 to the first pair in object 1, then compare all in 
+// object 2 to the second in object 1  and so on...)
+
+// If a pair matches return 'true', if none match return 'false'
+// Begin with match variable = false and reset only if match found
+
+function kvMatchCheck(object1, object2) {
+  
+  match = false;
+  
+  for (var key1 in object1) {
+  
+    for (var key2 in object2) {
+  	
+  	  if (key1 == key2 && object1[key1] == object2[key2]) {
+  	    match = true;
+      } 
+    }
+  }
+
+  return match;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // RELEASE 0 DRIVER TEST CODE 
+
+// console.log(longestString(["abc", "defghi", "jklmnopqr"]));
+
+// console.log(longestString(["abc", "defghi", "jklmno"]));
+
+// console.log(longestString(["Ruby", "I like to code!", "JavaScript"]));
+
+
+
+// RELEASE 1 DRIVER TEST CODE
+console.log()
+
+var steven = {name: "Steven", age: 54};
+var tamir =  {name: "Tamir", city: "Chicago", age: 54, height: 65};
+
+console.log("Test where Steven and Tamir share the age 54")
+console.log(kvMatchCheck(steven, tamir));
+
+console.log()
+
+var steven = {name: "Steven", age: 54};
+var tamir =  {name: "Tamir", city: "Chicago", age: 55, height: 65};
+
+console.log("Test again after Tamir's birthday, he's 55 now")
+console.log(kvMatchCheck(steven, tamir));
 
 
