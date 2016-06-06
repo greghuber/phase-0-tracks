4c20ae71ffdf1a70088df8cb6089669b99ed5cb8
greghuber <gregfhuber@gmail.com>
2016-06-05 19:56:57 -0500

    Populate the three tables with data

--- databases/challenge8_5/admin.rb
+++ databases/challenge8_5/admin.rb
@@ -12,12 +12,13 @@
 #  		CREATE, UPDATE & DELETE course records.
 
 require 'sqlite3'
+require 'faker'
 
 # CREATE 'grades' database
 db = SQLite3::Database.new("grade_data.db")
-db.results_as_hash = true
+# db.results_as_hash = true
 
-# CREATE 'students' table
+# ASSIGN variables code to create 'students', 'courses', and 'grades' tables
 create_students_table = <<-SQL
   CREATE TABLE IF NOT EXISTS students(
     id INTEGER PRIMARY KEY,
@@ -28,7 +29,6 @@ create_students_table = <<-SQL
   )
 SQL
 
-# CREATE 'courses' table
 create_courses_table = <<-SQL
   CREATE TABLE IF NOT EXISTS courses(
     id INTEGER PRIMARY KEY,
@@ -38,7 +38,6 @@ create_courses_table = <<-SQL
   )
 SQL
 
-# CREATE 'grades' table
 create_grades_table = <<-SQL
   CREATE TABLE IF NOT EXISTS grades(
     id INTEGER PRIMARY KEY,
@@ -49,7 +48,48 @@ create_grades_table = <<-SQL
   )
 SQL
 
-# Create 'students' and 'courses' tables
+# CREATE all tables passing in above code as arguments
 db.execute(create_students_table)
 db.execute(create_courses_table)
 db.execute(create_grades_table)
+
+#Write METHOD to ADD new student record
+def add_student(db, student_name, year, password)
+  db.execute("INSERT INTO students (name, year, good_standing, password) VALUES (student_name, year, true, password)")
+end
+
+# #Write METHOD to DELETE a student record
+# def delete_student(db, name)
+#   db.execute("DELETE FROM students WHERE attr=name")
+# end
+
+# #Write METHOD to UPDATE a student record
+# def delete_student(db, attribute, value, name)
+#   db.execute("DELETE FROM students WHERE attr=name")
+# end
+
+# UPDATE tablename SET attr02=newvalue02 WHERE attr01=value01
+
+# USER CODE to add student record, allow for admin to type name all lowercase
+
+puts "Enter student's name (First Last):"
+student_name = gets.chomp.split.map(&:capitalize).join(' ')
+puts "In what year is the student?  Enter a number:"
+year = gets.chomp.to_i
+
+add_student(db, student_name, year, Faker::Internet.password(8, 8))
+
+# USER CODE to delete student record
+
+# puts "Enter student's name (First Last):"
+# name = gets.chomp.split.map(&:capitalize).join(' ')
+
+
+
+
+
+#    ASSUMPTIONS MADE:
+# 1) Administrators can be trusted to enter data with no security concerns
+# 2) Grades of 'E' are the only reason for a student to fall out of 'good_standing'
+# 3) No one is looking over the student's shoulder as they enter their password
+
